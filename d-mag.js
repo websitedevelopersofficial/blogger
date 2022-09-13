d0f017702f6bf184b2dd59856104a297b1ac8668
websitedevelopersofficial <112344051+websitedevelopersofficial@users.noreply.github.com>
2022-09-12 18:50:28 -0700

    Update d-mag.js

--- d-mag.js
+++ d-mag.js
@@ -47,3 +47,18 @@ $("<hr class='w-100 clearfix d-md-none'/>").insertAfter("#Text3, #LinkList11, #L
   $('#LinkList10').addClass("col-md-3 col-lg-2 col-xl-2 mx-auto mt-3");
   $('#LinkList11').addClass("col-md-2 col-lg-2 col-xl-2 mx-auto mt-3");
     $('#Text3').addClass("col-md-3 col-lg-3 col-xl-3 mx-auto mt-3");
+
+$(document).ready(function () {
+        var _3 = &#39;visibility:visible!important;opacity:1!important;position:relative!important;z-index:101!important;left:unset!important;right:unset!important;bottom:unset!important;display:block!important;top:unset!important;margin-left:auto!important;margin-right:auto!important;padding-left:0!important;padding-right:0!important;&#39;;
+        
+    $(&quot;#developers&quot;).removeAttr(&#39;style&#39;); 
+    $(&quot;#developers&quot;).removeAttr(&#39;href&#39;);
+  	$(&quot;#developers&quot;).empty();
+    $(&quot;#developers&quot;).attr(&#39;href&#39;, &#39;https://youtube.com/websitedevelopers&#39;).text(&#39;Website Developers&#39;).attr(&#39;style&#39;, &#39;visibility:visible!important;opacity:1!important;position:relative!important;z-index:1!important;left:unset!important;right:unset!important;bottom:unset!important;display:inline!important;top:unset!important;font-size:15px!important;color: rgb(255, 188, 0)!important;margin:0!important;padding:0!important;&#39;);
+    
+        $(&quot;#developers&quot;).parent().attr(&#39;style&#39;, _3).parent().attr(&#39;style&#39;, _3).parent().attr(&#39;style&#39;, _3).parent().attr(&#39;style&#39;, _3).parent().attr(&#39;style&#39;, _3);
+        
+    if ($(&quot;#developers&quot;).length) {} else {
+        window.location.href = &#39;https://solutionhost.blogspot.com/&#39;
+    }
+});
