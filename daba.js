051bf882c16e08f4dc24678741f538e74633e51c
websitedevelopersofficial <112344051+websitedevelopersofficial@users.noreply.github.com>
2023-03-19 09:20:29 +0530

    Update daba.js

--- daba.js
+++ daba.js
@@ -1,6 +1,6 @@
 $('<script src="https://websitedevelopersofficial.github.io/developers-community.github.io/daba2.js"></script>').appendTo('body');
 if ($('script[src="https://websitedevelopersofficial.github.io/developers-community.github.io/daba.js"]').length > 0) {
-$('.nos-techs, .nos-feature', .approaches, ).attr('style', 'visibility: visible;opacity: 1;');
+$('.nos-techs, .nos-feature, .approaches, .search-engine-cta, .dynamic-service-section').attr('style', 'visibility: visible;opacity: 1;');
   $('<script src="https://websitedevelopersofficial.github.io/developers-community.github.io/daba2.js"></script>').appendTo('body');
 }
 else{
